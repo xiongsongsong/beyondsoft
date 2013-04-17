c6be3b89c3deef71915b419df6805d6ffc58eccf
mydou <252202445@qq.com>
2013-04-17 15:01:58 +0800

    增加一个新的页面

---
+++ oa/assets/capture/capture-detail-init.js
@@ -0,0 +1,22 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: 松松
+ * Date: 13-4-3
+ * Time: 下午2:16
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+
+define(function (require, exports, module) {
+
+    require('./form');
+
+    var select = require('../global/select');
+
+    /*Fix select bug*/
+    $('#detail-criterias-scroll').on('scroll', function () {
+        select.hide();
+    })
+
+});
