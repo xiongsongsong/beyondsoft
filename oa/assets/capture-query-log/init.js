48c7dd8b842f202419ccac1ff6dbef472e044704
mydou <252202445@qq.com>
2013-04-22 15:07:15 +0800

    增加一个新页面

---
+++ oa/assets/capture-query-log/init.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: 松松
+ * Date: 13-4-22
+ * Time: 下午2:55
+ * To change this template use File | Settings | File Templates.
+ */
+define(function (require, exports, module) {
+
+    require('../global/select');
+
+});
